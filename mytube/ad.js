e3ca326d3e953628f7021bba0f6ae4f5ab7821a7
billygoat891 <59946032+billygoat891@users.noreply.github.com>
2021-02-06 01:20:21 -0600

    .

---
+++ mytube/ad.js
@@ -0,0 +1,15 @@
+function main()
+{
+    var urlParams = new URLSearchParams(window.location.search);
+    if(!urlParams.has('video'))
+    {
+        document.write('<head><meta http-equiv="refresh" content="5; URL=index.html" /></head>');
+        return;
+    }
+
+    var videoUrlDecoded = urlParams.get('video').trim();
+    var vembed = document.getElementById('vembed');
+    vembed.setAttribute("src", `https://youtube.com/embed/${videoUrlDecoded}`);
+}
+
+main();
